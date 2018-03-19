1859145e53c7f41c3832d87287b27cae881ef582
El Mahraoui Omar (100) <omar.elmahraoui@cmc.be>
2018-03-19 21:52:03 +0100

    edited TestMergeConflict.java to test merge conflict

--- jar/src/main/java/TestMergeConflict.java
+++ jar/src/main/java/TestMergeConflict.java
@@ -2,6 +2,6 @@ public class TestMergeConflict {
     // added comment by omar
     private String mergeConflict;
     private String thisShouldCreateACnflict;
-    private String thisDefeatlyShouldCreateACnflict;
+    private String this_____DefeatlyShouldCreateACnflict;
 
 }
