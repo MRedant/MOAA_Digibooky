3266e9fa95d96f646300ffe772ec88813ac0b522
El Mahraoui Omar (100) <omar.elmahraoui@cmc.be>
2018-03-20 14:15:09 +0100

    working on bookservice

--- service/src/main/java/com/moaa/service/books/BookService.java
+++ service/src/main/java/com/moaa/service/books/BookService.java
@@ -1,9 +1,14 @@
 package com.moaa.service.books;
 
+import com.moaa.domain.books.BookRepository;
+
 import javax.inject.Inject;
 
 public class BookService {
+    
+    private final BookRepository bookRepository;
 
-   
-
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 }
