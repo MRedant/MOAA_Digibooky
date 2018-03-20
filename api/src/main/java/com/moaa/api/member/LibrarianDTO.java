d2f71ab7affe84660e1e5481cd0fe83507a74c53
AlexSchaillee <alex.schaillee@cm.be>
2018-03-20 15:39:59 +0100

    Story 9 done: Creating LibrarianService

---
+++ api/src/main/java/com/moaa/api/member/LibrarianDTO.java
@@ -0,0 +1,39 @@
+package com.moaa.api.member;
+
+import com.moaa.domain.member.memberInformation.Email;
+
+import java.util.UUID;
+
+public class LibrarianDTO {
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public static LibrarianDTO librarianDTO(){
+        return new LibrarianDTO();
+    }
+
+    public LibrarianDTO withFirstname(String firstName){
+        this.firstName = firstName;
+        return this;
+    }
+
+    public LibrarianDTO withLastName(String lastName){
+        this.lastName = lastName;
+        return this;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
--- domain/src/test/java/com/moaa/domain/member/LibrarianRepositoryTest.java
+++ domain/src/test/java/com/moaa/domain/member/LibrarianRepositoryTest.java
@@ -12,8 +12,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
-
 @RunWith(MockitoJUnitRunner.class)
 public class LibrarianRepositoryTest {
 
@@ -27,9 +25,9 @@ public class LibrarianRepositoryTest {
     public void unitTest_addLibrarian_GivenALibrarian_AddToLibrarianList() {
         //GIVEN
         Librarian librarian = Librarian.LibrarianBuilder.builder()
-                .setFirstName("Alex")
-                .setLastName("Schaillée")
-                .setEmail(Email.of("alex.schaillee@cm.be"))
+                .withFirstName("Alex")
+                .withLastName("Schaillée")
+                .withEmail(Email.of("alex.schaillee@cm.be"))
                 .createLibrarian();
         //WHEN
         Mockito.when(librarianDataBase.getLibrarianList())
