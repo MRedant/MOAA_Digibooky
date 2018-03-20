a82cf8f51b5a4d5748a17b10a057f016ad418891
MRedant <mredant@gmail.com>
2018-03-20 10:37:37 +0100

    domain - member - create admin, librarian and member

---
+++ domain/src/main/java/com/moaa/domain/member/Librarian.java
@@ -0,0 +1,67 @@
+package com.moaa.domain.member;
+
+import com.moaa.domain.member.utils.Email;
+
+import java.util.UUID;
+
+public class Librarian {
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private Email email;
+
+    Librarian(String firstName, String lastName, Email email) {
+        this.id = UUID.randomUUID();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public static class LibrarianBuilder {
+        private String firstName;
+        private String lastName;
+        private Email email;
+
+        public static LibrarianBuilder builder(){
+        return new LibrarianBuilder();
+        }
+
+        public LibrarianBuilder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public LibrarianBuilder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public LibrarianBuilder setEmail(Email email) {
+            this.email = email;
+            return this;
+        }
+
+        public Librarian createLibrarian() {
+            return new Librarian(firstName, lastName, email);
+        }
+
+
+    }
+}
